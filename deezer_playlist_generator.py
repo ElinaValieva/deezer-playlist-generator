4caf9ec585b63e9c0c5eb40b6a3034ce95bdf07a
ElinaValieva <veaufa@mail.ru>
2020-05-13 21:54:14 +0300

    Fix: result with limited tracks

--- deezer_playlist_generator.py
+++ deezer_playlist_generator.py
@@ -39,7 +39,7 @@ class DeezerPlayListCreator:
         for t in response_data:
             track = playlist.Track(t)
             if track.id not in playlist.playlist.get(artist_id):
-                result.append(track.title)
+                result.append(track)
         return result
 
     @staticmethod
@@ -50,17 +50,32 @@ class DeezerPlayListCreator:
                 artist.append(track.artist)
         return list(dict.fromkeys(artist))
 
+    @staticmethod
+    def __get_tracks(track_list, count_tracks):
+        result = []
+        index = 0
+        while len(result) != count_tracks:
+            for track in track_list:
+                result.append(track_list.get(track)[index])
+                if len(result) == count_tracks:
+                    break
+
+            if len(result) < count_tracks:
+                index += 1
+        return result
+
     def generate_tracks(self, user_id, count_tracks):
         user_playlist = self.__get_user_playlist(user_id)
-        all_tracks = []
+        all_tracks = {}
         user_artists = self.__get_user_artists(user_playlist)
         for artist in user_artists:
-            all_tracks.extend(self.__get_tracklist_by_artist(artist.id))
-        list(dict.fromkeys(all_tracks))
-        return all_tracks if len(all_tracks) < count_tracks else all_tracks[:count_tracks]
+            all_tracks[artist.name] = self.__get_tracklist_by_artist(artist.id)
+
+        return self.__get_tracks(all_tracks, count_tracks)
 
 
 if __name__ == '__main__':
     cp = DeezerPlayListCreator()
-    tracks = cp.generate_tracks(2149084062, 10)
-    print(tracks)
+    tracks = cp.generate_tracks(2149084062, 15)
+    for t in tracks:
+        print('{}-{}'.format(t.artist.name, t.title))
