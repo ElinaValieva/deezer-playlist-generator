89a4331b246c56e113d0fda4a0fd806cea58dfb3
ElinaValieva <veaufa@mail.ru>
2020-05-13 15:59:23 +0300

    Feat: initial track list generation

--- deezer_playlist_generator.py
+++ deezer_playlist_generator.py
@@ -25,8 +25,37 @@ def get_user_playlist():
     result = []
     for p in playlist_data:
         playlist_id = p.get('PLAYLIST_ID')
+        print(playlist_id)
         result.append(get_playlist(playlist_id))
     return result
 
 
-print(get_user_playlist())
+def get_tracklist_by_artist(artist_id):
+    response = requests.get('https://api.deezer.com/artist/' + str(artist_id) + '/top?limit=50')
+    if response.status_code != 200:
+        raise Exception('Error with loading playlist: {}'.format(response.reason))
+    response_data = response.json()['data']
+    result = []
+    for t in response_data:
+        result.append(playlist.Track(t))
+    return result
+
+
+def get_user_artists(user_playlist):
+    artist = []
+    for playlist in user_playlist:
+        for track in playlist.tracks:
+            artist.append(track.artist)
+    return artist
+
+
+def generate_tracks():
+    all_tracks = []
+    user_playlist = get_user_playlist()
+    user_artists = get_user_artists(user_playlist)
+    for artist in user_artists:
+        all_tracks.append(get_tracklist_by_artist(artist.id))
+    return all_tracks
+
+
+print(generate_tracks())
