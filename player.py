d45886706d483f348cb202067212b805009a2054
ElinaValieva <veaufa@mail.ru>
2020-05-14 14:15:27 +0300

    Refactor: make as a class

--- player.py
+++ player.py
@@ -6,93 +6,116 @@ from PIL import ImageTk, Image
 
 import deezer_playlist_generator
 
-player = tkr.Tk()
-player.title('Deezer player')
-player.geometry('422x420')
-player.configure(background='black')
-player.resizable(0, 0)
 
-current_song_number = 0
-music_list = {}
+class Downloader:
 
+    def download(self, soundtracks):
+        index = 0
+        music_list = {}
+        for track in soundtracks:
+            print('{}-{}'.format(track.artist.name, track.title))
+            music_list[index] = '{}-{}'.format(track.artist.name, track.title)
+            self.__download_file(track.preview, 'music/{}.mp3'.format(index))
+            self.__download_file(track.album.preview, 'album/{}.png'.format(index))
+            index += 1
+        return music_list
 
-def play():
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load('music/{}.mp3'.format(current_song_number))
-    pygame.mixer.music.play()
-
-
-def stop():
-    if pygame.mixer.get_init() is not None:
-        pygame.mixer.music.stop()
-
-
-def download(soundtracks):
-    index = 0
-    for track in soundtracks:
-        print('{}-{}'.format(track.artist.name, track.title))
-        music_list[index] = '{}-{}'.format(track.artist.name, track.title)
-        download_file(track.preview, 'music/{}.mp3'.format(index))
-        download_file(track.album.preview, 'album/{}.png'.format(index))
-        index += 1
+    @staticmethod
+    def __download_file(file, music_format):
+        response = requests.get(file)
+        file = open(music_format, 'wb')
+        file.write(response.content)
+        file.close()
 
 
-def download_file(file, music_format):
-    response = requests.get(file)
-    file = open(music_format, 'wb')
-    file.write(response.content)
-    file.close()
+class PlayerControl:
+
+    @staticmethod
+    def play(player_ui):
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load('music/{}.mp3'.format(player_ui.current_song_number))
+        pygame.mixer.music.play()
+
+    @staticmethod
+    def stop():
+        if pygame.mixer.get_init() is not None:
+            pygame.mixer.music.stop()
+
+    @staticmethod
+    def next_song(player_ui):
+        if player_ui.current_song_number < player_ui.max_size - 1:
+            player_ui.current_song_number += 1
+            PlayerControl.__init_sound_track(player_ui)
+
+    @staticmethod
+    def prev_song(player_ui):
+        if player_ui.current_song_number > 0:
+            player_ui.current_song_number -= 1
+            PlayerControl.__init_sound_track(player_ui)
+
+    @staticmethod
+    def __init_sound_track(player_ui):
+        player_ui.music_label.configure(text=player_ui.music_list.get(player_ui.current_song_number))
+        image = ImageTk.PhotoImage(Image.open('album/{}.png'.format(player_ui.current_song_number)))
+        player_ui.panel.configure(image=image)
+        player_ui.panel.image = image
+        player_ui.player.update_idletasks()
+        PlayerControl.stop()
+        PlayerControl.play(player_ui)
 
 
-def next_song():
-    global current_song_number
-    if current_song_number < 10:
-        current_song_number += 1
-        init_sound_track()
+class PlayerUI:
 
+    def __init__(self, music_max_size, music_list):
+        self.max_size = music_max_size
+        self.current_song_number = 0
+        self.music_list = music_list
 
-def prev_song():
-    global current_song_number
-    if current_song_number > 0:
-        current_song_number -= 1
-        init_sound_track()
+        # Player initialization
+        self.player = tkr.Tk()
+        self.player.title('Deezer player')
+        self.player.geometry('422x420')
+        self.player.configure(background='black')
+        self.player.resizable(0, 0)
 
+        # Button initialization
+        self.play_image = tkr.PhotoImage(file='images/play.png')
+        self.play_button = tkr.Button(text='Play', width=40, image=self.play_image,
+                                      command=lambda: PlayerControl.play(self))
 
-def init_sound_track():
-    musicLabel.configure(text=music_list.get(current_song_number))
-    image = ImageTk.PhotoImage(Image.open('album/{}.png'.format(current_song_number)))
-    panel.configure(image=image)
-    stop()
-    play()
+        self.stop_image = tkr.PhotoImage(file='images/stop.png')
+        self.stop_button = tkr.Button(text='Stop', width=40, image=self.stop_image, command=lambda: PlayerControl.stop)
 
+        self.next_image = tkr.PhotoImage(file='images/next.png')
+        self.next_button = tkr.Button(text='Next', width=40, image=self.next_image,
+                                      command=lambda: PlayerControl.next_song(self))
 
-playImage = tkr.PhotoImage(file='images/play.png')
-stopImage = tkr.PhotoImage(file='images/stop.png')
-nextImage = tkr.PhotoImage(file='images/next.png')
-prevImage = tkr.PhotoImage(file='images/prev.png')
+        self.prev_image = tkr.PhotoImage(file='images/prev.png')
+        self.prev_button = tkr.Button(text='Prev', width=40, image=self.prev_image,
+                                      command=lambda: PlayerControl.prev_song(self))
 
-playButton = tkr.Button(text='Play', width=40, image=playImage, command=play)
-stopButton = tkr.Button(text='Stop', width=40, image=stopImage, command=stop)
-nextButton = tkr.Button(text='Next', width=40, image=nextImage, command=next_song)
-prevButton = tkr.Button(text='Prev', width=40, image=prevImage, command=prev_song)
+        # Label initialization
+        self.album_image = ImageTk.PhotoImage(Image.open('album/{}.png'.format(self.current_song_number)))
+        self.panel = tkr.Label(self.player, image=self.album_image)
+        self.music_label = tkr.Label(text=music_list.get(self.current_song_number))
+        self.music_label.configure(font=("Comic Sans MS", 10), bg='black', foreground="white")
 
-img = ImageTk.PhotoImage(Image.open('images/album.jpg'))
-panel = tkr.Label(player, image=img)
+        # Grid configuration
+        self.panel.grid(columnspan=4, padx=(85, 20), pady=(20, 0))
+        self.music_label.grid(column=0, columnspan=4, ipadx=len(self.music_label['text']) * 2)
+        self.prev_button.grid(row=1, column=0, padx=(80, 10), pady=(80, 10))
+        self.play_button.grid(row=1, column=1, padx=(10, 10), pady=(80, 10))
+        self.stop_button.grid(row=1, column=2, padx=(10, 10), pady=(80, 10))
+        self.next_button.grid(row=1, column=3, padx=(10, 10), pady=(80, 10))
 
-musicLabel = tkr.Label(text='Undefined')
-musicLabel.configure(font=("Comic Sans MS", 10), bg='black', foreground="white")
+    def start(self):
+        self.player.mainloop()
 
-panel.grid(columnspan=4, padx=(85, 20), pady=(20, 0))
-musicLabel.grid(column=0, columnspan=4, ipadx=len(musicLabel['text']) * 2)
-prevButton.grid(row=1, column=0, padx=(80, 10), pady=(80, 10))
-playButton.grid(row=1, column=1, padx=(10, 10), pady=(80, 10))
-stopButton.grid(row=1, column=2, padx=(10, 10), pady=(80, 10))
-nextButton.grid(row=1, column=3, padx=(10, 10), pady=(80, 10))
 
 if __name__ == '__main__':
     creator = deezer_playlist_generator.DeezerPlayListCreator()
-    tracks = creator.generate_tracks(2149084062, 10)
-    download(tracks)
-    musicLabel.configure(text=music_list.get(0))
-    player.mainloop()
+    MAX_SIZE = 10
+    tracks = creator.generate_tracks(2149084062, MAX_SIZE)
+    sound_tracks = Downloader().download(tracks)
+    PlayerUI(MAX_SIZE, sound_tracks).start()
